f79dc795bb49da8f0e1bbfe4d1ed2d56cf02fb87
Samis-1 <samaralidya10@gmail.com>
2026-06-09 15:06:01 -0300

    1 & 2

---
+++ 2º etapa/Lab 10/exercicio_dois.py
@@ -0,0 +1,16 @@
+print("Die with a smilee if the world was ending I'll be next to u")
+for variavel in range(10):
+    print(variavel)
+# O resultado foi de zero até nove
+print("Espaço")
+for variavel in range(1,5):
+    print(variavel)
+#O pirnt foi de um até quatro
+print("Espaço")
+for variavel in range(0,10,2):
+    print(variavel)
+#O print foi pulando de dois em dois -> 0,2,4,6,8
+print("Espaço")
+for variavel in range(10,0,-1):
+    print(variavel)
+#Começou no dez e foi até o 1 -> contagem decrescente
