7cc296853b7e35f0894ec6f3b315f83419c6b3bf
Samis-1 <samaralidya10@gmail.com>
2026-07-01 15:19:50 -0300

    Segunda lista

---
+++ 2º etapa/2ª lista/Dois_b.py
@@ -0,0 +1,5 @@
+soma = 0
+fim = int(input())
+for i in range(1, fim, 2):
+    soma += i
+print(f"Soma: {soma}")
---
+++ 2º etapa/2ª lista/Um_a.py
@@ -0,0 +1,5 @@
+#Errado
+#for i in range(1,5)
+#print(i*2)
+for i in range(1,5):
+    print(i*2)
